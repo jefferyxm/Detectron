06caf17549cbb6b456a787fdc69b55e779bb634c
xiem <niudaoguajiao@163.com>
2018-11-28 10:55:52 +0800

    change RPN label generation IOU did not give imporvemnet 78.X%

--- test.sh
+++ test.sh
@@ -2,6 +2,7 @@
 export CUDA_VISIBLE_DEVICES=1
 python2 tools/test_net.py \
     --cfg configs/icdar/e2e_mask_rcnn_R-50-FPN_1x.yaml \
-    TEST.WEIGHTS ~/tmp/detectron-output/train/icdar_2015_train/generalized_rcnn/model_final.pkl \
+    TEST.WEIGHTS ~/icdar_tmp/2018_11_27_iou_0.5/train/icdar_2015_train/generalized_rcnn/model_final.pkl \
     NUM_GPUS 1 \
     VIS True
+
