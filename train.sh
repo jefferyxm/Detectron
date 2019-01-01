a87df29fceec78c74304a690152401b4b9439125
jeffery <1048023745@qq.com>
2019-01-01 21:30:51 +0800

    to do : path aggregation network  and feature map fusion

--- train.sh
+++ train.sh
@@ -1,7 +1,5 @@
 #! /bin/bash
 export CUDA_VISIBLE_DEVICES=0
 python2 tools/train_net.py \
-    --cfg configs/med/e2e_mask_rcnn_R-50-FPN_1x.yaml \
-    OUTPUT_DIR ~/med_tmp/detectron-output-rpn16 
-    
-    
+    --cfg configs/icdar/e2e_mask_rcnn_R-50-FPN_1x.yaml \
+    OUTPUT_DIR /home/xiem/1-deeplearning/model/detectron/debug
