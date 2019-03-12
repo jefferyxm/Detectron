85a80b8273c49d63e137fa9ca152879018c4760c
jefferyxm <abc@domain.com>
2019-03-12 11:10:37 +0800

    exp07002 fintune icdar 2015 based on exp07001

--- train.sh
+++ train.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
-export CUDA_VISIBLE_DEVICES=0
+export CUDA_VISIBLE_DEVICES=1
 python2 tools/train_net.py \
     --cfg configs/icdar/e2e_mask_rcnn_R-50-FPN_1x.yaml \
-    OUTPUT_DIR data/exp07001
+    OUTPUT_DIR data/exp07002
 
 
 # ---------------ada_exp01------------------- 
